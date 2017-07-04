f2a7266b954d6f17f7465e9ed3e08bc4917cea42
Shivam Bhagat <shivambhagat314@gmail.com>
2017-07-04 22:46:27 +0530

    Add files via upload

--- moc_dialogmat.cpp
+++ moc_dialogmat.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_dialogmat_t {
-    QByteArrayData data[10];
-    char stringdata0[162];
+    QByteArrayData data[7];
+    char stringdata0[92];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,17 +37,12 @@ QT_MOC_LITERAL(2, 32, 0), // ""
 QT_MOC_LITERAL(3, 33, 23), // "on_CustomButton_clicked"
 QT_MOC_LITERAL(4, 57, 7), // "checked"
 QT_MOC_LITERAL(5, 65, 21), // "on_z3img_valueChanged"
-QT_MOC_LITERAL(6, 87, 4), // "arg1"
-QT_MOC_LITERAL(7, 92, 22), // "on_radioButton_clicked"
-QT_MOC_LITERAL(8, 115, 23), // "on_radioButton_released"
-QT_MOC_LITERAL(9, 139, 22) // "on_radioButton_pressed"
+QT_MOC_LITERAL(6, 87, 4) // "arg1"
 
     },
     "dialogmat\0on_pushButton_clicked\0\0"
     "on_CustomButton_clicked\0checked\0"
-    "on_z3img_valueChanged\0arg1\0"
-    "on_radioButton_clicked\0on_radioButton_released\0"
-    "on_radioButton_pressed"
+    "on_z3img_valueChanged\0arg1"
 };
 #undef QT_MOC_LITERAL
 
@@ -57,7 +52,7 @@ static const uint qt_meta_data_dialogmat[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       8,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -65,24 +60,16 @@ static const uint qt_meta_data_dialogmat[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   54,    2, 0x08 /* Private */,
-       3,    0,   55,    2, 0x08 /* Private */,
-       3,    1,   56,    2, 0x08 /* Private */,
-       5,    1,   59,    2, 0x08 /* Private */,
-       7,    0,   62,    2, 0x08 /* Private */,
-       8,    0,   63,    2, 0x08 /* Private */,
-       9,    0,   64,    2, 0x08 /* Private */,
-       7,    1,   65,    2, 0x08 /* Private */,
+       1,    0,   34,    2, 0x08 /* Private */,
+       3,    0,   35,    2, 0x08 /* Private */,
+       3,    1,   36,    2, 0x08 /* Private */,
+       5,    1,   39,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, QMetaType::Bool,    4,
     QMetaType::Void, QMetaType::Double,    6,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void, QMetaType::Bool,    4,
 
        0        // eod
 };
@@ -94,7 +81,7 @@ void dialogmat::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, v
         Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->on_pushButton_clicked(); break;
-        case 7: _t->on_radioButton_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+
         default: ;
         }
     }
@@ -125,13 +112,13 @@ int dialogmat::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 8)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 8;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 8)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 8;
+        _id -= 4;
     }
     return _id;
 }
--- moc_mainwindow.cpp
+++ moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.8.0)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,17 +12,15 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'mainwindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.8.0. It"
+#error "This file was generated using the moc from 5.7.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
-QT_WARNING_PUSH
-QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[23];
-    char stringdata0[538];
+    QByteArrayData data[36];
+    char stringdata0[885];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -53,7 +51,20 @@ QT_MOC_LITERAL(18, 429, 18), // "on_rp_valueChanged"
 QT_MOC_LITERAL(19, 448, 25), // "on_frequency_valueChanged"
 QT_MOC_LITERAL(20, 474, 23), // "on_pushButton_4_clicked"
 QT_MOC_LITERAL(21, 498, 19), // "on_X1L_valueChanged"
-QT_MOC_LITERAL(22, 518, 19) // "on_X2L_valueChanged"
+QT_MOC_LITERAL(22, 518, 19), // "on_X2L_valueChanged"
+QT_MOC_LITERAL(23, 538, 22), // "on_Zinput_valueChanged"
+QT_MOC_LITERAL(24, 561, 23), // "on_ZinputY_valueChanged"
+QT_MOC_LITERAL(25, 585, 26), // "on_frequency2_valueChanged"
+QT_MOC_LITERAL(26, 612, 26), // "on_Inductance_valueChanged"
+QT_MOC_LITERAL(27, 639, 31), // "on_doubleSpinBox_9_valueChanged"
+QT_MOC_LITERAL(28, 671, 27), // "on_Capacitance_valueChanged"
+QT_MOC_LITERAL(29, 699, 19), // "on_btn_path_clicked"
+QT_MOC_LITERAL(30, 719, 21), // "on_pathButton_clicked"
+QT_MOC_LITERAL(31, 741, 25), // "on_pathStopButton_clicked"
+QT_MOC_LITERAL(32, 767, 29), // "on_cSeriesRadioButton_clicked"
+QT_MOC_LITERAL(33, 797, 28), // "on_cShuntRadioButton_clicked"
+QT_MOC_LITERAL(34, 826, 29), // "on_lSeriesRadioButton_clicked"
+QT_MOC_LITERAL(35, 856, 28) // "on_lShuntRadioButton_clicked"
 
     },
     "MainWindow\0on_btnReal_clicked\0\0"
@@ -71,7 +82,18 @@ QT_MOC_LITERAL(22, 518, 19) // "on_X2L_valueChanged"
     "on_pushButton_3_clicked\0on_rs_valueChanged\0"
     "on_rp_valueChanged\0on_frequency_valueChanged\0"
     "on_pushButton_4_clicked\0on_X1L_valueChanged\0"
-    "on_X2L_valueChanged"
+    "on_X2L_valueChanged\0on_Zinput_valueChanged\0"
+    "on_ZinputY_valueChanged\0"
+    "on_frequency2_valueChanged\0"
+    "on_Inductance_valueChanged\0"
+    "on_doubleSpinBox_9_valueChanged\0"
+    "on_Capacitance_valueChanged\0"
+    "on_btn_path_clicked\0on_pathButton_clicked\0"
+    "on_pathStopButton_clicked\0"
+    "on_cSeriesRadioButton_clicked\0"
+    "on_cShuntRadioButton_clicked\0"
+    "on_lSeriesRadioButton_clicked\0"
+    "on_lShuntRadioButton_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -81,7 +103,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      20,   14, // methods
+      33,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -89,26 +111,39 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  114,    2, 0x08 /* Private */,
-       3,    0,  115,    2, 0x08 /* Private */,
-       4,    0,  116,    2, 0x08 /* Private */,
-       5,    1,  117,    2, 0x08 /* Private */,
-       7,    0,  120,    2, 0x08 /* Private */,
-       8,    0,  121,    2, 0x08 /* Private */,
-       9,    1,  122,    2, 0x08 /* Private */,
-      10,    1,  125,    2, 0x08 /* Private */,
-      11,    1,  128,    2, 0x08 /* Private */,
-      12,    1,  131,    2, 0x08 /* Private */,
-      13,    1,  134,    2, 0x08 /* Private */,
-      14,    1,  137,    2, 0x08 /* Private */,
-      15,    1,  140,    2, 0x08 /* Private */,
-      16,    0,  143,    2, 0x08 /* Private */,
-      17,    1,  144,    2, 0x08 /* Private */,
-      18,    1,  147,    2, 0x08 /* Private */,
-      19,    1,  150,    2, 0x08 /* Private */,
-      20,    0,  153,    2, 0x08 /* Private */,
-      21,    1,  154,    2, 0x08 /* Private */,
-      22,    1,  157,    2, 0x08 /* Private */,
+       1,    0,  179,    2, 0x08 /* Private */,
+       3,    0,  180,    2, 0x08 /* Private */,
+       4,    0,  181,    2, 0x08 /* Private */,
+       5,    1,  182,    2, 0x08 /* Private */,
+       7,    0,  185,    2, 0x08 /* Private */,
+       8,    0,  186,    2, 0x08 /* Private */,
+       9,    1,  187,    2, 0x08 /* Private */,
+      10,    1,  190,    2, 0x08 /* Private */,
+      11,    1,  193,    2, 0x08 /* Private */,
+      12,    1,  196,    2, 0x08 /* Private */,
+      13,    1,  199,    2, 0x08 /* Private */,
+      14,    1,  202,    2, 0x08 /* Private */,
+      15,    1,  205,    2, 0x08 /* Private */,
+      16,    0,  208,    2, 0x08 /* Private */,
+      17,    1,  209,    2, 0x08 /* Private */,
+      18,    1,  212,    2, 0x08 /* Private */,
+      19,    1,  215,    2, 0x08 /* Private */,
+      20,    0,  218,    2, 0x08 /* Private */,
+      21,    1,  219,    2, 0x08 /* Private */,
+      22,    1,  222,    2, 0x08 /* Private */,
+      23,    1,  225,    2, 0x08 /* Private */,
+      24,    1,  228,    2, 0x08 /* Private */,
+      25,    1,  231,    2, 0x08 /* Private */,
+      26,    1,  234,    2, 0x08 /* Private */,
+      27,    1,  237,    2, 0x08 /* Private */,
+      28,    1,  240,    2, 0x08 /* Private */,
+      29,    0,  243,    2, 0x08 /* Private */,
+      30,    0,  244,    2, 0x08 /* Private */,
+      31,    0,  245,    2, 0x08 /* Private */,
+      32,    0,  246,    2, 0x08 /* Private */,
+      33,    0,  247,    2, 0x08 /* Private */,
+      34,    0,  248,    2, 0x08 /* Private */,
+      35,    0,  249,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -131,6 +166,19 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void, QMetaType::Double,    6,
     QMetaType::Void, QMetaType::Double,    6,
+    QMetaType::Void, QMetaType::Double,    6,
+    QMetaType::Void, QMetaType::Double,    6,
+    QMetaType::Void, QMetaType::Double,    6,
+    QMetaType::Void, QMetaType::Double,    6,
+    QMetaType::Void, QMetaType::Double,    6,
+    QMetaType::Void, QMetaType::Double,    6,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -145,6 +193,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 1: _t->on_btnImaginary_clicked(); break;
         case 2: _t->on_btnSuperimpose_clicked(); break;
         case 3: _t->on_doubleSpinBox_2_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+
         case 5: _t->on_pushButton_2_clicked(); break;
         case 6: _t->on_doubleSpinBox_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
         case 7: _t->on_doubleSpinBox_3_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
@@ -160,6 +209,19 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 17: _t->on_pushButton_4_clicked(); break;
         case 18: _t->on_X1L_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
         case 19: _t->on_X2L_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 20: _t->on_Zinput_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 21: _t->on_ZinputY_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 22: _t->on_frequency2_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 23: _t->on_Inductance_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+
+        case 25: _t->on_Capacitance_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 26: _t->on_btn_path_clicked(); break;
+        case 27: _t->on_pathButton_clicked(); break;
+        case 28: _t->on_pathStopButton_clicked(); break;
+        case 29: _t->on_cSeriesRadioButton_clicked(); break;
+        case 30: _t->on_cShuntRadioButton_clicked(); break;
+        case 31: _t->on_lSeriesRadioButton_clicked(); break;
+        case 32: _t->on_lShuntRadioButton_clicked(); break;
         default: ;
         }
     }
@@ -190,15 +252,14 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 20)
+        if (_id < 33)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 20;
+        _id -= 33;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 20)
+        if (_id < 33)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 20;
+        _id -= 33;
     }
     return _id;
 }
-QT_WARNING_POP
 QT_END_MOC_NAMESPACE
